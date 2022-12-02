912c1f379708f6fb341eed9bda097a3dbad40dca
Chris Turner <chris@thesegovia.com>
2022-12-02 10:08:58 +0000

    Day 2 Part 2 Solution

--- src/main/kotlin/uk/co/skipoles/adventofcode/day2/RockPaperScissors.kt
+++ src/main/kotlin/uk/co/skipoles/adventofcode/day2/RockPaperScissors.kt
@@ -8,10 +8,19 @@ enum class Choice {
   Scissors
 }
 
+enum class Action {
+  Win,
+  Lose,
+  Draw
+}
+
 data class Strategy(val opponentChoice: Choice, val playerChoice: Choice)
+
+data class Outcome(val opponentChoice: Choice, val playerAction: Action)
 
 object RockPaperScissors {
 
+  // Part 1 scoring implementation
   fun calculateScore(strategy: Strategy): Int = strategy.score() + strategy.playerChoice.score()
 
   private fun Strategy.score() =
@@ -31,6 +40,32 @@ object RockPaperScissors {
         Choice.Scissors -> 3
       }
 
+  // Part 2 scoring implementation
+  fun calculateScore(outcome: Outcome): Int =
+      calculateScore(Strategy(outcome.opponentChoice, outcome.playerChoice()))
+
+  private fun Outcome.playerChoice() =
+      when (this.playerAction) {
+        Action.Win -> opponentChoice.losesTo()
+        Action.Lose -> opponentChoice.winsOver()
+        Action.Draw -> opponentChoice
+      }
+
+  private fun Choice.losesTo() =
+      when (this) {
+        Choice.Rock -> Choice.Paper
+        Choice.Paper -> Choice.Scissors
+        Choice.Scissors -> Choice.Rock
+      }
+
+  private fun Choice.winsOver() =
+      when (this) {
+        Choice.Rock -> Choice.Scissors
+        Choice.Paper -> Choice.Rock
+        Choice.Scissors -> Choice.Paper
+      }
+
+  // Part 1 Version
   fun calculateScoreForStrategyGuide(filename: String): Int =
       File(filename).useLines { it.map(::lineToStrategy).map(::calculateScore).sum() }
 
@@ -46,8 +81,23 @@ object RockPaperScissors {
         'Z' -> Choice.Scissors
         else -> throw IllegalArgumentException("Unknown choice character: $this")
       }
+
+  // Part 2 Version
+  fun calculateScoreForStrategyGuide2(filename: String): Int =
+      File(filename).useLines { it.map(::lineToOutcome).map(::calculateScore).sum() }
+
+  private fun lineToOutcome(line: String) = Outcome(line[0].toChoice(), line[2].toAction())
+
+  private fun Char.toAction() =
+      when (this) {
+        'X' -> Action.Lose
+        'Y' -> Action.Draw
+        'Z' -> Action.Win
+        else -> throw IllegalArgumentException("Unknown action character: $this")
+      }
 }
 
 fun main() {
   println("Part 1: ${RockPaperScissors.calculateScoreForStrategyGuide("data/day2/input.txt")}")
+  println("Part 2: ${RockPaperScissors.calculateScoreForStrategyGuide2("data/day2/input.txt")}")
 }
--- src/test/kotlin/uk/co/skipoles/adventofcode/day2/RockPaperScissorsTest.kt
+++ src/test/kotlin/uk/co/skipoles/adventofcode/day2/RockPaperScissorsTest.kt
@@ -7,20 +7,21 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RockPaperScissorsTest {
 
+  // Tests for part 1
   @Test
-  fun `can calculate the strategy guide score for a single Win round`() {
+  fun `can calculate the score for a single Win round`() {
     val score = RockPaperScissors.calculateScore(Strategy(Choice.Rock, Choice.Paper))
     score shouldBe 8
   }
 
   @Test
-  fun `can calculate the strategy guide score for a single Loss round`() {
+  fun `can calculate the score for a single Loss round`() {
     val score = RockPaperScissors.calculateScore(Strategy(Choice.Paper, Choice.Rock))
     score shouldBe 1
   }
 
   @Test
-  fun `can calculate the strategy guide score for a single Draw round`() {
+  fun `can calculate the score for a single Draw round`() {
     val score = RockPaperScissors.calculateScore(Strategy(Choice.Scissors, Choice.Scissors))
     score shouldBe 6
   }
@@ -29,5 +30,30 @@ class RockPaperScissorsTest {
   fun `can calculate the score from a strategy guide file`() {
     val score = RockPaperScissors.calculateScoreForStrategyGuide("data/day2/test.txt")
     score shouldBe 15
+  }
+
+  // New tests introduced by part 2
+  @Test
+  fun `can calculate the score for a required Draw round`() {
+    val score = RockPaperScissors.calculateScore(Outcome(Choice.Rock, Action.Draw))
+    score shouldBe 4
+  }
+
+  @Test
+  fun `can calculate the score for a required Lose round`() {
+    val score = RockPaperScissors.calculateScore(Outcome(Choice.Paper, Action.Lose))
+    score shouldBe 1
+  }
+
+  @Test
+  fun `can calculate the score for a required Win round`() {
+    val score = RockPaperScissors.calculateScore(Outcome(Choice.Scissors, Action.Win))
+    score shouldBe 7
+  }
+
+  @Test
+  fun `can calculate the part 2 score from a strategy guide file`() {
+    val score = RockPaperScissors.calculateScoreForStrategyGuide2("data/day2/test.txt")
+    score shouldBe 12
   }
 }
